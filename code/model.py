9561f89b8c18b92844ba0b734dceb087281c84c4
yotammanne <yotammanne@mail.tau.ac.il>
2019-07-27 15:06:44 +0300

    model updates

--- code/model.py
+++ code/model.py
@@ -1,9 +1,113 @@
 
-from numpy import array
-from keras.preprocessing.text import one_hot
-from keras.preprocessing.sequence import pad_sequences
+from process_data import *
+from keras.layers.core import Dropout, Dense
+from keras.layers import Conv1D
+from keras.layers.pooling import MaxPooling1D
+from keras.layers import LSTM
+from keras.layers import RepeatVector
+from keras.layers import TimeDistributed
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Flatten
 from keras.layers.embeddings import Embedding
+from keras.utils.vis_utils import plot_model
+import numpy as np
 
+
+EMBEDDING_DIM = 100
+MAX_LEN_OF_SENTENCE = 192
+filter_sizes = (2, 3, 4)
+BATCH_SIZE = 32
+WIKI_SIMPLE = '/normal.aligned'
+WIKI_NORMAL = '/simple.aligned'
+GLOVE_PATH = 'C:\\Users\\guyazov\\PycharmProjects\\SentenceSimplificationProject\\data\\Glove\\glove.6B.100d.txt'
+# wiki_data_path = 'C:\\Users\\guyazov\\PycharmProjects\\SentenceSimplificationProject\\data\\wiki'
+wiki_data_path = '../data/wiki'
+
+def load_dataset(normal_sents, simple_sents,dataset_size):
+
+    # load training set and test set
+    #Cut the datasets randomly
+    rand_idx = np.random.choice(len(normal_sents),size=dataset_size)
+    normal_sents = np.array(normal_sents)[rand_idx]
+    simple_sents = np.array(simple_sents)[rand_idx]
+    normal_sents_train, simple_sents_train, normal_sents_test, simple_sents_test = split_data(normal_sents, simple_sents)
+    return normal_sents_train, simple_sents_train, normal_sents_test, simple_sents_test
+
+
+def Vectorize_Sentences(data, max_len,unk_vec):
+    list_of_matrices = np.zeros((len(data),max_len,EMBEDDING_DIM))
+    for i, sent in enumerate(data):
+        mat = create_sentence_matrix(embedding_matrix, embeddings_matrix_index, sent, unk_vec, max_len)
+        list_of_matrices[i] = mat
+    return list_of_matrices
+
+
+def define_model(normal_sent_dataset_size, simple_sent_dataset_size, normal_max_len, simple_max_len, n_units):
+    model = Sequential()
+    # Need to add here an embadding layer, if we decide to use the tokenizer. In addition if we
+    # decide so we need to change this function a little bit.
+    model.add(Embedding(normal_sent_dataset_size, n_units, input_length=normal_max_len))
+    model.add(Conv1D(filters=64, kernel_size=3, activation='relu', padding='valid'))
+    model.add(Dropout(0.2))
+    model.add(MaxPooling1D(pool_size=2))
+    #model.add(Flatten())
+    #model.add(Dense(simple_sent_dataset_size,activation='relu'))
+    model.add(LSTM(64))
+    model.add(RepeatVector(simple_max_len))
+    model.add(LSTM(n_units, return_sequences=True))
+    model.add(TimeDistributed(Dense(simple_sent_dataset_size, activation='softmax')))
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+# evaluate the skill of the model
+def evaluate_model(model, tokenizer, sources, normal_sents_orig, simple_sents_orig):
+    actual, predicted = list(), list()
+    for i, source in enumerate(sources):
+        # translate encoded source text
+        source = source.reshape((1, source.shape[0]))
+        translation = predict_sequence(model, tokenizer, source)
+        raw_target, raw_src = simple_sents_orig[i], normal_sents_orig[i]
+        if i < 10:
+            print('src=[%s], target=[%s], predicted=[%s]' % (raw_src, raw_target, translation))
+        #actual.append([raw_target.split()])
+        #predicted.append(translation.split())
+    # calculate BLEU score
+    #print('BLEU-1: %f' % corpus_bleu(actual, predicted, weights=(1.0, 0, 0, 0)))
+    #print('BLEU-2: %f' % corpus_bleu(actual, predicted, weights=(0.5, 0.5, 0, 0)))
+    #print('BLEU-3: %f' % corpus_bleu(actual, predicted, weights=(0.3, 0.3, 0.3, 0)))
+    #print('BLEU-4: %f' % corpus_bleu(actual, predicted, weights=(0.25, 0.25, 0.25, 0.25)))
+
+if __name__ == '__main__':
+    normal_sents_orig, simple_sents_orig = load_data(wiki_data_path, None)
+    #embedding_matrix, embeddings_matrix_index = build_embedding_matrix(GLOVE_PATH)
+    normal_sents_train, simple_sents_train, normal_sents_test, simple_sents_test = load_dataset(normal_sents_orig, simple_sents_orig,10)
+    #unk_vec = create_vector_unknown_token()
+    max_len_normal = max_input_sentece_length(normal_sents_orig)
+    max_len_simple = max_output_sentece_length(simple_sents_orig)
+    #normal_sents_train_as_matrices = Vectorize_Sentences(normal_sents_train,max_len_normal,unk_vec)
+    #simple_sents_train_as_matrices = Vectorize_Sentences(simple_sents_train,max_len_simple,unk_vec)
+    #normal_sents_test_as_matrices = Vectorize_Sentences(normal_sents_test,max_len_normal,unk_vec)
+    #simple_sents_test_as_matrices = Vectorize_Sentences(simple_sents_test,max_len_simple,unk_vec)
+
+    # From here down is a trying of the tokenizer
+    normal_tokenizer = create_tokenizer(normal_sents_orig)
+    simple_tokenizer = create_tokenizer(simple_sents_orig)
+    normal_max_len = max_input_sentece_length(normal_sents_orig)
+    simple_max_len = max_output_sentece_length(simple_sents_orig)
+    # prepare training data
+    train_noraml = encode_sequences(normal_tokenizer, normal_max_len, normal_sents_train)
+    train_simple = encode_sequences(simple_tokenizer, simple_max_len, simple_sents_train)
+    train_simple = encode_output(train_simple, len(simple_tokenizer.word_index) + 1)
+    # prepare validation data
+    test_noraml = encode_sequences(normal_tokenizer, normal_max_len, normal_sents_test)
+    test_simple = encode_sequences(simple_tokenizer, simple_max_len, simple_sents_test)
+    test_simple = encode_output(test_simple, len(simple_tokenizer.word_index) + 1)
+    model = define_model(len(normal_tokenizer.word_index) + 1, len(simple_tokenizer.word_index) + 1, normal_max_len, simple_max_len, 64)
+    # summarize defined model
+    print(model.summary())
+    plot_model(model, to_file='model.png', show_shapes=True)
+    # fit network
+    # epochs, batch_size, verbose = 1, 32, 1
+    # model.fit(train_noraml, train_simple, epochs=epochs, batch_size=batch_size, verbose=verbose)
+    # # test on some training sequences
+    # print('train')
+    # evaluate_model(model, simple_tokenizer, train_noraml, normal_sents_orig, simple_sents_orig)
--- code/process_data.py
+++ code/process_data.py
@@ -1,11 +1,14 @@
 import numpy as np
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
 
 
 WIKI_SIMPLE = '/normal.aligned'
 WIKI_NORMAL = '/simple.aligned'
-GLOVE_PATH = '/home/yotam/personal/NLP/Project/code/glove.6B.100d.txt'
+GLOVE_PATH = 'C:\\Users\\guyazov\\PycharmProjects\\SentenceSimplificationProject\\data\\Glove\\glove.6B.100d.txt'
 BATCH_SIZE = 32
-
+EMBEDDING_DIM = 100
 def load_data(wiki_path,newsela_path):
     '''
     load raw text from data files for normal and simple sentences
@@ -22,8 +25,8 @@ def load_data(wiki_path,newsela_path):
 		'poor work',
 		'Could have done better.']
     '''
-    f_wiki_simple = open(wiki_path + WIKI_SIMPLE,'r')
-    f_wiki_normal = open(wiki_path + WIKI_NORMAL,'r')
+    f_wiki_simple = open(wiki_path + WIKI_SIMPLE,'r',encoding="utf8")
+    f_wiki_normal = open(wiki_path + WIKI_NORMAL,'r',encoding="utf8")
 
     normal_sents = f_wiki_normal.readlines()
     simple_sents = f_wiki_simple.readlines()
@@ -44,6 +47,45 @@ def load_data(wiki_path,newsela_path):
 
     return normal_sents,simple_sents
 
+
+def create_vector_unknown_token():
+    '''
+    :return: a vector for unknown token
+    '''
+    # Get number of vectors and hidden dim
+    with open(GLOVE_PATH, 'r', encoding="utf8") as f:
+        for i, line in enumerate(f):
+            pass
+    n_vec = i + 1
+    hidden_dim = len(line.split(' ')) - 1
+
+    vecs = np.zeros((n_vec, hidden_dim), dtype=np.float32)
+
+    with open(GLOVE_PATH, 'r', encoding="utf8") as f:
+        for i, line in enumerate(f):
+            vecs[i] = np.array([float(n) for n in line.split(' ')[1:]], dtype=np.float32)
+
+    average_vec = np.mean(vecs, axis=0)
+    return average_vec
+
+def split_data(normal_sents,simple_sents):
+    '''
+
+    :param normal_sents:
+    :param simple_sents:
+    :return: training set and test set
+    '''
+
+    shuffle_indices = np.random.permutation(np.arange(len(normal_sents)))
+    normal_sents = np.array(normal_sents)[shuffle_indices]
+    simple_sents = np.array(simple_sents)[shuffle_indices]
+    train_len = int(len(normal_sents) * 0.9)
+    normal_sents_train = normal_sents[:train_len]
+    simple_sents_train = simple_sents[:train_len]
+    normal_sents_test = normal_sents[train_len:]
+    simple_sents_test = simple_sents[train_len:]
+
+    return normal_sents_train, simple_sents_train, normal_sents_test, simple_sents_test
 
 def build_embedding_matrix(glove_path):
     '''
@@ -97,16 +139,18 @@ def get_vocab_size(embedding_matrix):
 
 
 
-def create_sentence_matrix(embedding_matrix,embeddings_matrix_index,sentence,max_sent_len):
+def create_sentence_matrix(embedding_matrix,embeddings_matrix_index,sentence,unk_vector, max_len):
     '''
     embedding a sentence to a matrix of size (max_sent_len, embedding_dim)
     '''
-    sent_matrix = np.zeros((max_sent_len, embedding_matrix.shape[1]))
+
+    #sent_matrix = []
+    sent_matrix = np.zeros((max_len, embedding_matrix.shape[1]))
     for i, word in enumerate(sentence):
-        if word in embeddings_matrix_index:
-            sent_matrix[i] = embedding_matrix[embeddings_matrix_index[word]]
+        if word.lower() in embeddings_matrix_index:
+            sent_matrix[i] = np.array(embedding_matrix[embeddings_matrix_index[word.lower()]])
         else:
-            sent_matrix[i] = embedding_matrix[embeddings_matrix_index['<unk>']]
+            sent_matrix[i] = np.array(unk_vector)
     return sent_matrix
 
 
@@ -132,11 +176,63 @@ def create_batch_matrix(embedding_matrix, embeddings_matrix_index, normal_sents,
     return batch,simplified
 
 
+# fit a tokenizer
+def create_tokenizer(lines):
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(lines)
+    return tokenizer
+
+# encode and pad sequences
+def encode_sequences(tokenizer, length, lines):
+    # integer encode sequences
+    X = tokenizer.texts_to_sequences(lines)
+    # pad sequences with 0 values
+    X = pad_sequences(X, maxlen=length, padding='post')
+    return X
+
+# one hot encode target sequence
+def encode_output(sequences, vocab_size):
+    ylist = list()
+    for sequence in sequences:
+        encoded = to_categorical(sequence, num_classes=vocab_size)
+        ylist.append(encoded)
+    y = np.array(ylist)
+    y = y.reshape(sequences.shape[0], sequences.shape[1], vocab_size)
+    return y
+
+# map an integer to a word
+def word_for_id(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == integer:
+            return word
+    return None
+
+# generate target given source sequence
+def predict_sequence(model, tokenizer, source):
+    prediction = model.predict(source, verbose=0)[0]
+    integers = [np.argmax(vector) for vector in prediction]
+    target = list()
+    for i in integers:
+        word = word_for_id(i, tokenizer)
+        if word is None:
+            break
+        target.append(word)
+    return ' '.join(target)
 
 if __name__=='__main__':
-    normal_sents,simple_sents = load_data('/home/yotam/personal/NLP/Project/data/wiki',None)
+    '''
+    normal_sents,simple_sents = load_data('C:\\Users\\guyazov\\PycharmProjects\\SentenceSimplificationProject\\data\\wiki',None)
     embedding_matrix, embeddings_matrix_index = build_embedding_matrix(GLOVE_PATH)
     max_len = max_input_sentece_length(normal_sents)
     sent = ["guy", "is", "great", "in", "soccer"]
     mat = create_sentence_matrix(embedding_matrix,embeddings_matrix_index,sent,max_len)
+    
+    '''
+    normal_sents, simple_sents = load_data('C:\\Users\\guyazov\\PycharmProjects\\SentenceSimplificationProject\\data\\wiki', None)
+    normal_sents_train, simple_sents_train, normal_sents_test, simple_sents_test = split_data(normal_sents, simple_sents)
+    normal_tokenizer = create_tokenizer(normal_sents)
+    simple_tokenizer = create_tokenizer(simple_sents)
+    normal_max_len = max_input_sentece_length(normal_sents)
+    simple_max_len = max_output_sentece_length(simple_sents)
+
     print('d')
