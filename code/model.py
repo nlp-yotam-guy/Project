71897e094364db252ef86d0dc445dd38ae15a683
guyazov <guyazov@gmail.com>
2019-07-29 19:52:57 +0300

    TEST

--- code/model.py
+++ code/model.py
@@ -3,12 +3,13 @@ from process_data import *
 from keras.layers.core import Dropout, Dense
 from keras.layers import Conv1D
 from keras.layers.pooling import MaxPooling1D
-from keras.layers import LSTM
+from keras.layers import LSTM, Bidirectional, concatenate, Flatten
 from keras.layers import RepeatVector
 from keras.layers import TimeDistributed
 from keras.models import Sequential
 from keras.layers.embeddings import Embedding
 from keras.utils.vis_utils import plot_model
+from keras import Model, Input
 import numpy as np
 
 
@@ -43,20 +44,41 @@ def Vectorize_Sentences(data, max_len,unk_vec):
 
 def define_model(normal_sent_dataset_size, simple_sent_dataset_size, normal_max_len, simple_max_len, n_units):
     model = Sequential()
-    # Need to add here an embadding layer, if we decide to use the tokenizer. In addition if we
-    # decide so we need to change this function a little bit.
     model.add(Embedding(normal_sent_dataset_size, n_units, input_length=normal_max_len))
     model.add(Conv1D(filters=64, kernel_size=3, activation='relu', padding='valid'))
-    model.add(Dropout(0.2))
+    model.add(Dropout(0.5))
     model.add(MaxPooling1D(pool_size=2))
-    #model.add(Flatten())
-    #model.add(Dense(simple_sent_dataset_size,activation='relu'))
     model.add(LSTM(64))
     model.add(RepeatVector(simple_max_len))
-    model.add(LSTM(n_units, return_sequences=True))
+    model.add(Bidirectional(LSTM(n_units, return_sequences=True)))
     model.add(TimeDistributed(Dense(simple_sent_dataset_size, activation='softmax')))
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
+
+
+def define_model2(normal_sent_dataset_size, simple_sent_dataset_size, normal_max_len, simple_max_len, n_units):
+    deep_inputs = Input(shape=(normal_max_len,))
+    input_layer = Embedding(normal_sent_dataset_size, n_units, input_length=normal_max_len)(deep_inputs)
+    conv1 = Conv1D(100, (3), activation='relu')(input_layer)
+    dropout_1 = Dropout(0.5)(conv1)
+    conv2 = Conv1D(100, (4), activation='relu')(input_layer)
+    dropout_2 = Dropout(0.5)(conv2)
+    conv3 = Conv1D(100, (5), activation='relu')(input_layer)
+    dropout_3 = Dropout(0.5)(conv3)
+    conv4 = Conv1D(100, (6), activation='relu')(input_layer)
+    dropout_4 = Dropout(0.5)(conv4)
+    maxpool1 = MaxPooling1D(pool_size=normal_max_len - 2)(dropout_1)
+    maxpool2 = MaxPooling1D(pool_size=normal_max_len - 3)(dropout_2)
+    maxpool3 = MaxPooling1D(pool_size=normal_max_len - 4)(dropout_3)
+    maxpool4 = MaxPooling1D(pool_size=normal_max_len - 5)(dropout_4)
+    cc1 = concatenate([maxpool1,maxpool2,maxpool3,maxpool4], axis=2)
+    lstm = LSTM(236, return_sequences=True)(cc1)
+    output = Dense(simple_sent_dataset_size, activation='softmax')(lstm)
+    model = Model(inputs=[deep_inputs], outputs=output)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    #print(model.summary())
+    return model
+
 
 # evaluate the skill of the model
 def evaluate_model(model, tokenizer, sources, normal_sents_orig, simple_sents_orig):
@@ -77,18 +99,18 @@ def evaluate_model(model, tokenizer, sources, normal_sents_orig, simple_sents_or
     #print('BLEU-4: %f' % corpus_bleu(actual, predicted, weights=(0.25, 0.25, 0.25, 0.25)))
 
 if __name__ == '__main__':
-    normal_sents_orig, simple_sents_orig = load_data(wiki_data_path, None)
+
     #embedding_matrix, embeddings_matrix_index = build_embedding_matrix(GLOVE_PATH)
-    normal_sents_train, simple_sents_train, normal_sents_test, simple_sents_test = load_dataset(normal_sents_orig, simple_sents_orig,10)
+
     #unk_vec = create_vector_unknown_token()
-    max_len_normal = max_input_sentece_length(normal_sents_orig)
-    max_len_simple = max_output_sentece_length(simple_sents_orig)
     #normal_sents_train_as_matrices = Vectorize_Sentences(normal_sents_train,max_len_normal,unk_vec)
     #simple_sents_train_as_matrices = Vectorize_Sentences(simple_sents_train,max_len_simple,unk_vec)
     #normal_sents_test_as_matrices = Vectorize_Sentences(normal_sents_test,max_len_normal,unk_vec)
     #simple_sents_test_as_matrices = Vectorize_Sentences(simple_sents_test,max_len_simple,unk_vec)
 
     # From here down is a trying of the tokenizer
+    normal_sents_orig, simple_sents_orig = load_data(wiki_data_path, None)
+    normal_sents_train, simple_sents_train, normal_sents_test, simple_sents_test = load_dataset(normal_sents_orig, simple_sents_orig, 10)
     normal_tokenizer = create_tokenizer(normal_sents_orig)
     simple_tokenizer = create_tokenizer(simple_sents_orig)
     normal_max_len = max_input_sentece_length(normal_sents_orig)
@@ -101,13 +123,17 @@ if __name__ == '__main__':
     test_noraml = encode_sequences(normal_tokenizer, normal_max_len, normal_sents_test)
     test_simple = encode_sequences(simple_tokenizer, simple_max_len, simple_sents_test)
     test_simple = encode_output(test_simple, len(simple_tokenizer.word_index) + 1)
-    model = define_model(len(normal_tokenizer.word_index) + 1, len(simple_tokenizer.word_index) + 1, normal_max_len, simple_max_len, 64)
+    #model2 = define_model2(len(normal_tokenizer.word_index) + 1, len(simple_tokenizer.word_index) + 1, normal_max_len, simple_max_len, 64)
     # summarize defined model
-    print(model.summary())
-    plot_model(model, to_file='model.png', show_shapes=True)
+    #print(model2.summary())
+    #plot_model(model2, to_file='model.png', show_shapes=True)
+    #model2.fit(train_noraml, train_simple, epochs=1, batch_size=32, verbose=1)
     # fit network
+    model = define_model(len(normal_tokenizer.word_index) + 1, len(simple_tokenizer.word_index) + 1, normal_max_len,
+                         simple_max_len, 64)
     epochs, batch_size, verbose = 1, 32, 1
     model.fit(train_noraml, train_simple, epochs=epochs, batch_size=batch_size, verbose=verbose)
+    print(model.summary())
     # # test on some training sequences
-    print('train')
-    evaluate_model(model, simple_tokenizer, train_noraml, normal_sents_orig, simple_sents_orig)
+    #print('train')
+    #evaluate_model(model, simple_tokenizer, train_noraml, normal_sents_orig, simple_sents_orig)
