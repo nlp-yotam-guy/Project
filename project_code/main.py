152c0d6a604c5ac5da3b45d8c0388f5c49687353
yotammanne <yotammanne@mail.tau.ac.il>
2019-08-03 19:20:51 +0300

    fixed output dimetions bug, fixed typo

--- project_code/main.py
+++ project_code/main.py
@@ -15,7 +15,7 @@ MAX_LEN_OF_SENTENCE = 192
 FILTER_SIZES = (2, 3, 4)
 BATCH_SIZE = 32
 NUM_EPOCHES = 10
-LIMIT_DATA_SIZE = 10000
+LIMIT_DATA_SIZE = 1000
 WIKI_SIMPLE = '../data/wiki/normal.aligned'
 WIKI_NORMAL = '../data/wiki/simple.aligned'
 GLOVE_PATH = 'C:\\Users\\guyazov\\PycharmProjects\\SentenceSimplificationProject\\data\\Glove\\glove.6B.100d.txt'
@@ -38,13 +38,13 @@ def main():
     normal_max_len = max_input_sentece_length(normal_sents_orig)
     simple_max_len = max_output_sentece_length(simple_sents_orig)
     # prepare training data
-    train_noraml = encode_sequences(tokenizer, normal_max_len, normal_sents_train)
+    train_normal = encode_sequences(tokenizer, normal_max_len, normal_sents_train)
     train_simple = encode_sequences(tokenizer, simple_max_len, simple_sents_train)
     train_simple = encode_output(train_simple, len(tokenizer.word_index) + 1)
     # prepare validation data
     test_normal = encode_sequences(tokenizer, normal_max_len, normal_sents_test)
     test_simple = encode_sequences(tokenizer, simple_max_len, simple_sents_test)
-    test_simple = encode_output(test_simple, len(tokenizer.word_index) + 1)
+    test_simple = encode_output(test_simple, embedding_matrix.shape[0])
     # fit network
     print('Creating a model')
     model = Rephraser(EMBEDDING_DIM, embedding_matrix, normal_max_len, DROP_PROB, HIDDEN_SIZE,
@@ -53,10 +53,10 @@ def main():
 
     #plot_model(model, to_file='model.png', show_shapes=True)
     print('Fitting the model')
-    model.train(train_noraml, train_simple, VALIDATION_SPLIT)
+    model.train(train_normal, train_simple, VALIDATION_SPLIT)
     # # test on some training sequences
     print('Training the model')
-    model.evaluate(tokenizer, train_noraml, normal_sents_orig, simple_sents_orig)
+    model.evaluate(tokenizer, train_normal, normal_sents_orig, simple_sents_orig)
 
 
 if __name__ == '__main__':
