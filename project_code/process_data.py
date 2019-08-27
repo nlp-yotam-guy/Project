2b4df2c617d541b33e17f9e1caff8d6cafdff40d
guyazov <guyazov@gmail.com>
2019-08-27 15:25:51 +0300

    glove take 2

--- project_code/process_data.py
+++ project_code/process_data.py
@@ -173,8 +173,8 @@ def build_embedding_matrix(glove_path, vocab, use_cuda):
         values = line.split()
         word = values[0]
         coefs = np.asarray(values[1:], dtype='float32')
-        coefs = Variable(torch.LongTensor(coefs))
-        coefs = coefs.cuda() if use_cuda else coefs
+        #coefs = Variable(torch.LongTensor(coefs))
+        #coefs = coefs.cuda() if use_cuda else coefs
         embeddings_index[word] = coefs
     f.close()
     print('Loaded %s word vectors.' % len(embeddings_index))
