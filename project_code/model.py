ea232983f2c1cdd99bbd0f4edbbfa1e248f2a524
guyazov <guyazov@gmail.com>
2019-08-31 17:13:17 +0300

    Debuging

--- project_code/model.py
+++ project_code/model.py
@@ -422,8 +422,8 @@ class Rephraser:
             decoder_output, decoder_hidden = \
                 self.decoder(prev_word, decoder_output, decoder_hidden, cnn_a, cnn_c, input_variable, out_length, self.vocab_simple)
 
+            print("dec_out : ", decoder_output)
             topv, topi = decoder_output.data.topk(1)
-            print("topi : ", topi)
             ni = topi[0][0].item()
             target_sent.append(self.vocab_simple.id2word[ni])
             prev_word = Variable(torch.LongTensor([[ni]]))
