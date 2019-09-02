0e5a1927518defa83dec4ab187583a0f9458f452
yotammanne <yotammanne@mail.tau.ac.il>
2019-09-02 08:32:21 +0300

    evaluation bug

--- project_code/model.py
+++ project_code/model.py
@@ -418,18 +418,19 @@ class Rephraser:
         out_length = 0
         word_count = dict()
         while not ni == 1 and out_length < self.max_len:
+            decoder_hidden = decoder_hidden[-1].view(1, 1, -1)
             decoder_output, decoder_hidden = \
                 self.decoder(prev_word, decoder_output, decoder_hidden, cnn_a, cnn_c, input_variable, out_length, self.vocab_simple)
             # print("softmax: ", decoder_output)
             # print("top: ", decoder_output.data.topk(1))
             # print(self.vocab_simple.id2word[ni])
             topv, topi = decoder_output.data.topk(1)
-            ni = topi[0][0].item()
+            ni = topi[0].item()
             target_sent.append(self.vocab_simple.id2word[ni])
             prev_word = Variable(torch.LongTensor([[ni]]))
             prev_word = prev_word.cuda() if self.use_cuda else prev_word
             out_length += 1
-            decoder_output = torch.mm(decoder_output, self.embedding_matrix_simple)
+            # decoder_output = torch.mm(decoder_output, self.embedding_matrix_simple)
 
 
         orig_sent = word_id_to_sent(sent_pair[0], self.vocab_normal)
