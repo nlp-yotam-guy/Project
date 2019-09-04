977e26c1df6561c7c38806acc711311e805172d5
yotammanne <yotammanne@mail.tau.ac.il>
2019-09-04 23:17:29 +0300

    no display bug

--- project_code/main.py
+++ project_code/main.py
@@ -74,20 +74,18 @@ def main():
 
     # summarize history for accuracy
     plt.plot(history.history['acc'])
-    plt.plot(history.history['val_acc'])
     plt.title('model accuracy')
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
+    plt.legend(['train'], loc='upper left')
     #plt.show()
-    plt.savefig('accuracy,png')
+    plt.savefig('accuracy.png')
     # summarize history for loss
     plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
     plt.title('model loss')
     plt.ylabel('loss')
     plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
+    plt.legend(['train'], loc='upper left')
     #plt.show()
     plt.savefig('loss.png')
 
